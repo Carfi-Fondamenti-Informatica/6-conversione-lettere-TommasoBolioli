0b27eab35e8b7d24b8a4904df1c320917558a2d3
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-11-29 21:36:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+//
+// Created by tomma on 29/11/2021.
+//
 
+#include "lib.h"
+
+char funzione (char let){
+char res;
+    if ((let >= 'a' && let <= 'z') || (let>='A' && let<='Z')) {
+        if (let >= 'a' && let <= 'z'){
+           res= let - 32;
+           return res;
+        }else if(let>='A' && let<='Z'){
+            res=let + 32;
+            return res;
+        }
+    }else{
+    return '0';
+    }}

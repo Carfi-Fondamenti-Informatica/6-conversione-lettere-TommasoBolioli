eb06fa3b97da0711e900b18f90b302de509b4829
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-11-29 21:34:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
 int main(){
-  return 0;
+    char let;
+    cin >> let;
+    if ((let >= 'a' && let <= 'z') || (let>='A' && let<='Z')){
+    char res1= funzione(let);
+    cout << res1 << endl;
+}else{
+    cout << "errore"<<endl;}
 }
